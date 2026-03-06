894892ff3b073cc50f99499880364e145dc578ad
NguyenSonTung16 <162577055+NguyenSonTung16@users.noreply.github.com>
2026-03-06 11:52:20 +0700

    feat:add courses lists by using grid and gap css

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const button = document.querySelector('.button-link');
 button.addEventListener('click', () => {
     alert('Dang ky thanh cong!');
 });
+const courseButtons = document.querySelectorAll('.course-button');
+courseButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Dang ky khoa hoc thanh cong!');
+    });
+});
