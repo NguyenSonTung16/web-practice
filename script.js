4da63431b53e496748fe3e6ccf919cc5fcfa85b0
NguyenSonTung16 <162577055+NguyenSonTung16@users.noreply.github.com>
2026-03-06 19:59:24 +0700

    feat:add header part on top of website

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ courseButtons.forEach(button => {
         alert('Dang ky khoa hoc thanh cong!');
     });
 });
+const menu_items = document.querySelectorAll('.menu-item');
+menu_items.forEach(item => {
+    item.addEventListener('click', () => {
+        alert('Chuyen tiep sang trang moi!');
+    });
+});
+const navbar=document.querySelector('.header-navbar');
+navbar.addEventListener('click',()=>{
+    alert('Chuyen tiep sang trang moi!');
+});
